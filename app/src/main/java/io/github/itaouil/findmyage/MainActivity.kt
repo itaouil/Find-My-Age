cd1609cb0924232ea1b4117b61a1db980e92aacb
itaouil <itaouil95@gmail.com>
2017-09-30 15:46:19 +0100

    Added age computation.

--- app/src/main/java/io/github/itaouil/findmyage/MainActivity.kt
+++ app/src/main/java/io/github/itaouil/findmyage/MainActivity.kt
@@ -2,11 +2,46 @@ package io.github.itaouil.findmyage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Display main activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        // Button event (click)
+        buFindAge.setOnClickListener {
+
+            // Fetch data of birth from input
+            val dob:Int = dob.text.toString().toInt()
+
+            // Compute user's age
+            val age:Int = Calendar.getInstance().get(Calendar.YEAR) - dob
+
+            // Display age in the text view
+            ageView.text = "Your age is $age"
+        }
+        */
+
     }
+
+    fun findAge(view: View) {
+
+        // Fetch data of birth from input
+        val dob:Int = dob.text.toString().toInt()
+
+        // Compute user's age
+        val age:Int = Calendar.getInstance().get(Calendar.YEAR) - dob
+
+        // Display age in the text view
+        ageView.text = "Your age is $age"
+
+    }
+
 }
